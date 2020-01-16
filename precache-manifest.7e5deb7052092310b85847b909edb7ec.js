2f5e7e0737ec346ced94fe2ebbb6387882bf01dc
Yehor Markevych <mr.yegor@yahoo.com>
2020-01-16 15:22:26 +0200

    Updates

---
+++ precache-manifest.7e5deb7052092310b85847b909edb7ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e415905ef895b23a0dfe9e28d5d54d",
+    "url": "/travelyo-app/index.html"
+  },
+  {
+    "revision": "34d35cce7f045fb1d748",
+    "url": "/travelyo-app/static/css/2.5ffa1744.chunk.css"
+  },
+  {
+    "revision": "0748edc17727768f1edd",
+    "url": "/travelyo-app/static/css/main.68e3df3c.chunk.css"
+  },
+  {
+    "revision": "34d35cce7f045fb1d748",
+    "url": "/travelyo-app/static/js/2.4595be4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/travelyo-app/static/js/2.4595be4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0748edc17727768f1edd",
+    "url": "/travelyo-app/static/js/main.83bfa1be.chunk.js"
+  },
+  {
+    "revision": "8f01c2284b108cc2f012",
+    "url": "/travelyo-app/static/js/runtime-main.0c6530a7.js"
+  }
+]);
